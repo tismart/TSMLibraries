c8a039efe2584113c50fa8d735c48fb75975a168
luis_alo7 <luisalo.rios@gmail.com>
2015-06-11 09:19:35 -0500

    Merge branch 'master' of https://github.com/tismart/TSMLibraries
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	myapplication/src/main/java/com/tismart/myapplication/MainActivity.java

--- tsmlytics/src/main/java/com/tismart/tsmlytics/TSMLytics.java
+++ tsmlytics/src/main/java/com/tismart/tsmlytics/TSMLytics.java
@@ -176,57 +176,57 @@ public class TSMLytics {
     }
 
     /**
-     *APP INFO<br>
-     * Obtiene las aplicaciones abiertas actuales en el dispositivo.<br>
-     * Si la versión de Android es mayor o igual al API 21 este método devolverá null<br>
-     * Si la versión de Android es menor al API 21 este método te devolverá una lista del tipo App<br>
-     * App.Name nombre de la aplicación en String<br>
-     * App.Icon ícono de la aplicación en Drawable<br>
-     * App.Package nombre del paquete de la aplicación en String<br>
-     *<p>
-     *DEVICE INFO<br>
-     * Obtiene los estados de algunos elmentos del dispositivo. Este método siempre devuelve un dato diferente a null<br>
-     * Device.Baterry  = Porcentaje de batería en float<br>
-     * Device.IsTablet = True or False en boolean<br>
-     * Device.Model = Modelo del dispositivo en String<br>
-     * Device.ID = ID del dispositivo Android.Secure.ID en String<br>
-     * Device.IsRooted = Si el dispositivo ha sido rooteado en boolean<br>
-     *<p>
-     * DISK INFO<br>
-     * Obtiene la información del disco interno del dispositivo. Si hubiera una SD-Card conectada también obtiene la información de ella.<br>
-     * Si hay algún error el método devolverá null<br>
-     * Disk.InternalFree = Disco interno libre en bytes long<br>
-     * Disk.InternalUsed = Disco interno usado en bytes long<br>
-     * Disk.InternalTotal = Disco interno total en bytes long<br>
-     * Disk.ExternalFree = Disco externo libre en bytes long<br>
-     * Disk.ExternalUsed = Disco externo usado en bytes long<br>
-     * Disk.ExternalTotal = Disco externo total en bytes long<br>
-     *<p>
-     *MEMORY INFO<br>
-     * Obtiene la información de la memoria del dispositivo.<br>
-     * Si hay algún error el método devolverá null<br>
-     * Memory.Free = Memoria libre en bytes long<br>
-     * Memory.Used = Memoria usado en bytes long<br>
-     * Memory.Total = Memoria total en bytes long<br>
-     *<p>
-     *NETWORK INFO<br>
-     * Obtiene la información de la red del dispositivo.<br>
-     * Si hay algún error el método devolverá null<br>
-     * Network.Connection = Modo de conexión Wi-Fi, Mobile, Ethernet, Bluetooth en String<br>
-     * Network.Type = Tipo de conexión Wi-Fi, 3G, Edge entre otros en String<br>
-     * Network.Streght = Intensidad en la conexión. Siempre es un número negativo en String<br>
-     *<p>
-     *OS INFO<br>
-     * Obtiene la información del dispositivo. Este método siempre devuelve un dato diferente a null<br>
-     * OS.Name = Nombre del sistema operativo en String<br>
-     * OS.Version = Versión del sistema operativo en String<br>
-     *<p>
-     *SCREEN INFO<br>
-     * Obtiene la información de la pantalla del dispositivo.<br>
-     * Si hay algún error el método devolverá null<br>
-     * Screen.Density = Densidad de la pantalla en int<br>
-     * Screen.Size = Tamaño de la pantalla en float<br>
-     * Screen.Orientation = Orientación de la pantalla en String<br>
+     * <p>APP INFO</p>
+     * <p>Obtiene las aplicaciones abiertas actuales en el dispositivo.</p>
+     * <p>Si la versión de Android es mayor o igual al API 21 este método devolverá null</p>
+     * <p>Si la versión de Android es menor al API 21 este método te devolverá una lista del tipo App</p>
+     * <p>App.Name nombre de la aplicación en String</p>
+     * <p>App.Icon ícono de la aplicación en Drawable</p>
+     * <p>App.Package nombre del paquete de la aplicación en String</p>
+     * <p> </p>
+     * <p>DEVICE INFO</p>
+     * <p>Obtiene los estados de algunos elmentos del dispositivo. Este método siempre devuelve un dato diferente a null</p>
+     * <p>Device.Baterry  = Porcentaje de batería en float</p>
+     * <p>Device.IsTablet = True or False en boolean</p>
+     * <p>Device.Model = Modelo del dispositivo en String</p>
+     * <p>Device.ID = ID del dispositivo Android.Secure.ID en String</p>
+     * <p>Device.IsRooted = Si el dispositivo ha sido rooteado en boolean</p>
+     * <p> </p>
+     * <p>DISK INFO</p>
+     * <p>Obtiene la información del disco interno del dispositivo. Si hubiera una SD-Card conectada también obtiene la información de ella.</p>
+     * <p>Si hay algún error el método devolverá null</p>
+     * <p>Disk.InternalFree = Disco interno libre en bytes long</p>
+     * <p>Disk.InternalUsed = Disco interno usado en bytes long</p>
+     * <p>Disk.InternalTotal = Disco interno total en bytes long</p>
+     * <p>Disk.ExternalFree = Disco externo libre en bytes long</p>
+     * <p>Disk.ExternalUsed = Disco externo usado en bytes long</p>
+     * <p>Disk.ExternalTotal = Disco externo total en bytes long</p>
+     * <p> </p>
+     * <p>MEMORY INFO</p>
+     * <p>Obtiene la información de la memoria del dispositivo.</p>
+     * <p>Si hay algún error el método devolverá null</p>
+     * <p>Memory.Free = Memoria libre en bytes long</p>
+     * <p>Memory.Used = Memoria usado en bytes long</p>
+     * <p>Memory.Total = Memoria total en bytes long</p>
+     * <p> </p>
+     * <p>NETWORK INFO</p>
+     * <p>Obtiene la información de la red del dispositivo.</p>
+     * <p>Si hay algún error el método devolverá null</p>
+     * <p>Network.Connection = Modo de conexión Wi-Fi, Mobile, Ethernet, Bluetooth en String</p>
+     * <p>Network.Type = Tipo de conexión Wi-Fi, 3G, Edge entre otros en String</p>
+     * <p>Network.Streght = Intensidad en la conexión. Siempre es un número negativo en String</p>
+     * <p> </p>
+     * <p>OS INFO</p>
+     * <p>Obtiene la información del dispositivo. Este método siempre devuelve un dato diferente a null</p>
+     * <p>OS.Name = Nombre del sistema operativo en String</p>
+     * <p>OS.Version = Versión del sistema operativo en String</p>
+     * <p> </p>
+     * <p>SCREEN INFO</p>
+     * <p>Obtiene la información de la pantalla del dispositivo.</p>
+     * <p>Si hay algún error el método devolverá null</p>
+     * <p>Screen.Density = Densidad de la pantalla en int</p>
+     * <p>Screen.Size = Tamaño de la pantalla en float</p>
+     * <p>Screen.Orientation = Orientación de la pantalla en String</p>
      * @return
      */
     public HashMap<TSMLyticsEnum, Object> getAllWithEntities() {
