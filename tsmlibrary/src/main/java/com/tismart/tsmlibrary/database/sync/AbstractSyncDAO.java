3be5606437251936e25139dd81050729feedcd6a
luis.burgos <luis.burgos@tismartperu.com>
2015-08-18 12:17:47 -0500

    Creación de carpetas sync.

---
+++ tsmlibrary/src/main/java/com/tismart/tsmlibrary/database/sync/AbstractSyncDAO.java
@@ -0,0 +1,150 @@
+package com.tismart.tsmlibrary.database.sync;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.tismart.tsmlibrary.database.annotations.Elemento;
+import com.tismart.tsmlibrary.database.annotations.Entidad;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@SuppressWarnings({"unused", "WeakerAccess", "UnusedReturnValue"})
+public abstract class AbstractSyncDAO<T> {
+
+    final private String tableName;
+    final private Class<T> clase;
+    final private SQLiteDatabase db = SyncDatabaseInstance.getInstance().getDatabase();
+    protected HashMap<String, Integer> lstColumnIndex;
+    private HashMap<Field, Elemento> lstFieldElemento;
+    private String columnFlag;
+
+    //region Inicialización
+    protected AbstractSyncDAO(Class<T> entidad, String columnFlag) {
+        Entidad annotation = entidad.getAnnotation(Entidad.class);
+        tableName = annotation.tableName().length() > 0 ? annotation.tableName() : entidad.getSimpleName();
+        clase = entidad;
+        this.columnFlag = columnFlag;
+
+        inicializeFieldElement();
+        inicializeColumnIndex();
+    }
+
+    private void inicializeFieldElement() {
+        if (lstFieldElemento == null) {
+            lstFieldElemento = new HashMap<>();
+
+            for (Field f : clase.getDeclaredFields()) {
+                f.setAccessible(true);
+                if (f.isAnnotationPresent(Elemento.class)) {
+                    lstFieldElemento.put(f, f.getAnnotation(Elemento.class));
+                }
+            }
+        }
+    }
+
+    private void inicializeColumnIndex() {
+        if (lstColumnIndex == null) {
+            lstColumnIndex = new HashMap<>();
+            for (Elemento elemento : lstFieldElemento.values()) {
+                lstColumnIndex.put(elemento.columnName(), -1);
+            }
+        }
+    }
+
+    //endregion
+
+    /**
+     * @param flagValue Para listar los elementos con el valor designado.
+     * @return Listado con las entidades.
+     */
+    public ArrayList<T> listEntities(String flagValue) {
+        ArrayList<T> lst = new ArrayList<>();
+        Cursor cursor = null;
+        try {
+            cursor = db.query(
+                    tableName,
+                    null,
+                    columnFlag + " like '" + flagValue + "'",
+                    null,
+                    null,
+                    null,
+                    null,
+                    null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                do {
+                    lst.add(transformCursorToEntity(cursor));
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return lst;
+    }
+
+    T transformCursorToEntity(Cursor cursor) {
+        try {
+            T entidad = clase.newInstance();
+            for (Field f : clase.getDeclaredFields()) {
+                f.setAccessible(true);
+                if (lstFieldElemento.containsKey(f)) {
+                    Elemento elemento = lstFieldElemento.get(f);
+
+                    if (lstColumnIndex.get(elemento.columnName()) == -1) {
+                        lstColumnIndex.put(elemento.columnName(), cursor.getColumnIndex(elemento.columnName()));
+                    }
+
+                    switch (elemento.elementType()) {
+                        case LONG:
+                            f.setLong(entidad, transformCursorToLong(elemento.columnName(), cursor));
+                            break;
+                        case BLOB:
+                            break;
+                        case DOUBLE:
+                            f.setDouble(entidad, transformCursorToDouble(elemento.columnName(), cursor));
+                            break;
+                        case BOOLEAN:
+                            f.setBoolean(entidad, transformCursorToBoolean(elemento.columnName(), cursor));
+                            break;
+                        case STRING:
+                            f.set(entidad, transformCursorToString(elemento.columnName(), cursor));
+                            break;
+                        case INTEGER:
+                            f.setInt(entidad, transformCursorToInt(elemento.columnName(), cursor));
+                            break;
+                    }
+                }
+            }
+            return entidad;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    private int transformCursorToInt(String name, Cursor cursor) {
+        return cursor.isNull(lstColumnIndex.get(name)) ? 0 : cursor.getInt(lstColumnIndex.get(name));
+    }
+
+    private double transformCursorToDouble(String name, Cursor cursor) {
+        return cursor.isNull(lstColumnIndex.get(name)) ? 0d : cursor.getDouble(lstColumnIndex.get(name));
+    }
+
+    private long transformCursorToLong(String name, Cursor cursor) {
+        return cursor.isNull(lstColumnIndex.get(name)) ? 0l : cursor.getLong(lstColumnIndex.get(name));
+    }
+
+    private String transformCursorToString(String name, Cursor cursor) {
+        return cursor.isNull(lstColumnIndex.get(name)) ? "" : cursor.getString(lstColumnIndex.get(name));
+    }
+
+    private boolean transformCursorToBoolean(String name, Cursor cursor) {
+        return transformCursorToInt(name, cursor) == 1;
+    }
+}
---
+++ tsmlibrary/src/main/java/com/tismart/tsmlibrary/database/sync/SyncDatabaseInstance.java
@@ -0,0 +1,72 @@
+package com.tismart.tsmlibrary.database.sync;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.tismart.tsmlibrary.database.DatabaseNotFoundException;
+import com.tismart.tsmlibrary.database.SQLDatabaseHelper;
+
+import java.io.IOException;
+
+/**
+ * Created by Luis Miguel on 23/01/2015.
+ * <p/>
+ * Clase que usa el patrón singleton para instanciar el helper que comunica la aplicación con la bd.
+ */
+
+@SuppressWarnings({"unused", "SameParameterValue"})
+public class SyncDatabaseInstance {
+    private static SyncDatabaseInstance ourInstance = null;
+    final private Context context;
+    private final String db_name;
+    private final int db_version;
+    public boolean existDatabase = false;
+    private SQLiteDatabase db;
+    private SQLDatabaseHelper dbHelper;
+
+    private SyncDatabaseInstance(Context context, String db_name, int db_version, boolean fromAssets) {
+        dbHelper = new SQLDatabaseHelper(context, db_name, db_version);
+        this.context = context;
+        this.db_name = db_name;
+        this.db_version = db_version;
+        try {
+            dbHelper.createDatabase(fromAssets);
+            existDatabase = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DatabaseNotFoundException dnfe) {
+            existDatabase = false;
+        }
+    }
+
+    public static void initialize(Context context, String db_name, int db_version, boolean fromAssets) {
+        ourInstance = new SyncDatabaseInstance(context, db_name, db_version, fromAssets);
+    }
+
+    public static SyncDatabaseInstance getInstance() {
+        return ourInstance;
+    }
+
+    public SQLDatabaseHelper getDBHelper() {
+        if (dbHelper == null) {
+            dbHelper = new SQLDatabaseHelper(context, db_name, db_version);
+        }
+        return dbHelper;
+    }
+
+    public SQLiteDatabase getDatabase() {
+        if (db == null) {
+            db = getDBHelper().getWritableDatabase();
+        }
+        return db;
+    }
+
+    public boolean existsDatabase() {
+        existDatabase = getDBHelper().existsDatabase();
+        return existDatabase;
+    }
+
+    public void deleteDatabase() {
+        context.deleteDatabase(db_name);
+    }
+}
